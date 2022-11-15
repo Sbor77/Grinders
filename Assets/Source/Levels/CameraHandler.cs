5acf12fcf74985076b558a0ba29770162a9e60e5
Sergey Borovkov <sborovkov82@mail.ru>
2022-11-15 20:34:46 +0300

    Change camera logic at start, add box with cross prefab

--- Assets/Source/Levels/CameraHandler.cs
+++ Assets/Source/Levels/CameraHandler.cs
@@ -9,28 +9,41 @@ public class CameraHandler : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera _bigboxCamera;
     [SerializeField] private CinemachineVirtualCamera _playCamera;
+    [SerializeField] private Joystick _josytick;
     [SerializeField] private float _playCameraPositionY;    
     [SerializeField] private Vector3 _targetBigboxCameraPosition;
-
-    private float _bigboxCameraTime = 3f;
+    [SerializeField] private float _bigboxCameraTime;
+    [SerializeField] private float _bigboxCameraDelay;
 
     private void Start()
     {
-        MoveBigboxCamera();   
+        MoveBigboxCamera();
+
+        SetJoystickActive(false);
     }
 
     private void Activate (CinemachineVirtualCamera camera)
     {
-        camera.VirtualCameraGameObject.SetActive(true);       
+        camera.Priority = 1;        
+    }
+
+    private void SetJoystickActive(bool active)
+    {
+        _josytick.gameObject.SetActive(active);
     }
 
     private void MoveBigboxCamera()
     {
-        _bigboxCamera.transform.DOMove(_targetBigboxCameraPosition, _bigboxCameraTime).OnComplete(() => Deactivate(_bigboxCamera));
+        _bigboxCamera.transform.DOMove(_targetBigboxCameraPosition, _bigboxCameraTime).OnComplete(() =>
+        {
+            Deactivate(_bigboxCamera, _bigboxCameraDelay);
+
+            DOVirtual.DelayedCall(_bigboxCameraTime, () => SetJoystickActive(true));
+        });
     }
 
-    private void Deactivate(CinemachineVirtualCamera camera)
+    private void Deactivate(CinemachineVirtualCamera camera, float delay = 0)
     {
-        camera.VirtualCameraGameObject.SetActive(false);
+        DOVirtual.DelayedCall(delay, () => camera.Priority = 0);        
     }
 }
