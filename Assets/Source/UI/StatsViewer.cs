ac66eff8fffa299fb6f716585dddc7951b5c87b0
Aleksandr Bashkirov <alxx85@mail.ru>
2022-11-17 00:53:20 +0700

    Add shop scene and player heal logic

---
+++ Assets/Source/UI/StatsViewer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StatsViewer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _stageText;
+    [SerializeField] private TMP_Text _goldText;
+    [SerializeField] private TMP_Text _killedText;
+    [SerializeField] private TMP_Text _healthText;
+    [SerializeField] private TMP_Text _speedText;
+
+    private void Init()
+    {
+        
+    }
+}
