e9cfa0b2fad5c95f9f4a69cd3d51773b6467739c
Sergey Borovkov <sborovkov82@mail.ru>
2022-11-18 13:56:57 +0300

    Finish level1 settings

--- Assets/Source/Levels/Level.cs
+++ Assets/Source/Levels/Level.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
@@ -11,6 +12,14 @@ public class Level : MonoBehaviour
     [SerializeField] private DoorOpener _bigboxDoor;
     [SerializeField] private CameraHandler _cameraHandler;
     [SerializeField] private EnemySpawner _enemySpawner;
+
+    private int _currentLevel;
+
+    private int _ShopScene = 0;
+    private int _levelOneScene = 1;
+    private int _levelTwoScene = 2;
+    private int _levelThreeScene = 3;
+    private int _levelFourScene = 4;
 
     private QuestInfo _missionConditions;    
     private int _currentCoins;
@@ -22,6 +31,8 @@ public class Level : MonoBehaviour
     private void Start()
     {
         _missionConditions = _infoViewer.MissionConditions;
+
+        
     }
 
     private void OnEnable()
@@ -72,9 +83,16 @@ public class Level : MonoBehaviour
     {
         _cameraHandler.ZoomInPlayCamera();
 
-        DOVirtual.DelayedCall(2f, () =>
-        {            
-            _finalEffects.PlayAllEffects();
-        });
+        DOVirtual.DelayedCall(2f, () => _finalEffects.PlayAllEffects());
+
+        DOVirtual.DelayedCall(_finalEffects.Duration, () => LoadScene(_levelTwoScene));
+
+    }
+
+    private void LoadScene (int sceneIndex)
+    {
+        print("загружаем сцену = " + sceneIndex);
+
+        SceneManager.LoadScene(sceneIndex);
     }
 }
