a1e7a83f92ebaba8e2195c4d3889a8f2a89220e1
Aleksandr Bashkirov <alxx85@mail.ru>
2022-11-23 20:16:07 +0700

    Fix show money count on level shop

--- Assets/Source/UI/Shop/StatsViewer.cs
+++ Assets/Source/UI/Shop/StatsViewer.cs
@@ -13,7 +13,7 @@ public class StatsViewer : MonoBehaviour
     {
         _stageText.text = $"Level: {DataHandler.Instance.GetSavedLevel()}";
 
-        _goldText.text = $"Money: {DataHandler.Instance.GetSavedLevelMoney()}";
+        _goldText.text = $"Money: {DataHandler.Instance.GetSavedTotalMoney()}";
 
         _killedText.text = $"Killed: {DataHandler.Instance.GetSavedKills()}";
 
