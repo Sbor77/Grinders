2a40de173d0b63d998ce7fbec1622061397bcf4d
Sergey Borovkov <sborovkov82@mail.ru>
2022-11-28 21:54:56 +0300

    Add AOE charge effect, fix AOE effects timig, change tutorial panel

--- Assets/Source/Levels/AreaAttack.cs
+++ Assets/Source/Levels/AreaAttack.cs
@@ -15,11 +15,16 @@ public class AreaAttack : MonoBehaviour
     [SerializeField] private AudioSource _explosionSound;
     [SerializeField] private AudioSource _chargeSound;
     [Space]
+    [SerializeField] private GameObject _chargeRotator;
+    [SerializeField] private ParticleSystem _chargeEffect;
+    [Space]
     [SerializeField] private float _radius;
     [SerializeField] private float _damage;
     [SerializeField] private float _chargeDuration;
         
     private float _effectHeiht = 0.05f;
+    private float _radiusOffset = 2;
+    private Vector3 _rotationAroundY = new Vector3(0, 360, 0);
     private Vector3 _effectPosition;
     
 
@@ -28,6 +33,24 @@ public class AreaAttack : MonoBehaviour
         _collider.enabled = false;
 
         _collider.radius = _radius;
+
+        
+    }
+
+    private void SetChargeEffectPosition()
+    {
+        _chargeEffect.transform.localPosition = new Vector3(0, _effectHeiht, - (_radius + _radiusOffset));
+    }
+
+    private void ShowChargeEffect()
+    {
+        float effectLiveTime = 1.5f;
+
+        _chargeEffect.gameObject.SetActive(true);
+
+        _chargeRotator.transform.DORotate(_rotationAroundY, _chargeDuration, RotateMode.FastBeyond360).SetEase(Ease.Linear);
+
+        DOVirtual.DelayedCall(effectLiveTime, () => _chargeEffect.gameObject.SetActive(false));     
     }
 
     private void DamageTargets()
@@ -62,14 +85,20 @@ public class AreaAttack : MonoBehaviour
 
         transform.position = _effectPosition;        
 
+        SetChargeEffectPosition();
+
         Sequence animation = DOTween.Sequence();
 
         _auraEffect.gameObject.SetActive(true);
+        
+        animation.AppendCallback(() => 
+        {
+            ShowChargeEffect();
+            _chargeSound.Play();            
+        });
+        animation.Join(_auraEffect.transform.DOScale(_radius, _chargeDuration).SetEase(Ease.Linear));
 
-        animation.Append(_auraEffect.transform.DOScale(_radius, _chargeDuration).SetEase(Ease.Linear));
-        animation.AppendCallback(_chargeSound.Play);
-
-        animation.AppendInterval(_chargeDuration);
+        animation.AppendInterval(0.5f);
         animation.AppendCallback(() =>
         {
             _auraEffect.gameObject.SetActive(false);
@@ -77,7 +106,7 @@ public class AreaAttack : MonoBehaviour
             _explosionEffect.gameObject.SetActive(true);            
         });
 
-        animation.AppendInterval(0.5f);
+        animation.AppendInterval(_chargeDuration);
         animation.AppendCallback( () =>
         {
             _explosionSound.Play();
