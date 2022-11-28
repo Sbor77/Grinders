2f385c16e7a781fd5dc0a2696a0fa462d4915e53
Sergey Borovkov <sborovkov82@mail.ru>
2022-11-28 16:36:26 +0300

    Add tutorial UI

--- Assets/Source/UI/TutorialPanel.cs
+++ Assets/Source/UI/TutorialPanel.cs
@@ -7,30 +7,46 @@ using DG.Tweening;
 
 public class TutorialPanel : MonoBehaviour
 {
-    [SerializeField] private QuestPanel _questPanel;
+    [SerializeField] private GameObject _infoPanel;
+    [Space]    
+    [SerializeField] private Image _missionPad;
+    [SerializeField] private Image _attackPad;    
     [Space]
-    [SerializeField] private TMP_Text _coinText;
-    [SerializeField] private Image _coinArrow;
-    [SerializeField] private Image _coinPad;
-    [Space]
-    [SerializeField] private TMP_Text _killText;
-    [SerializeField] private Image _killArrow;
-    [SerializeField] private Image _killPad;
-    [Space]
-    [SerializeField] private TMP_Text _bigboxText;
-    [SerializeField] private Image _bigboxArrow;
-    [SerializeField] private Image _bigboxPad;
-    [SerializeField] private Image _bigboxMark;
-    [Space]
+    [SerializeField] private TMP_Text _targetGuides;
+    [SerializeField] private TMP_Text _bigboxGuides;
+    [Space]    
     [SerializeField] private GameObject _playerGuides;
+    [SerializeField] private TMP_Text _walkGuides;
+    [SerializeField] private TMP_Text _walkGuidesMobile;
+    [SerializeField] private TMP_Text _attackGuides;
+    [SerializeField] private TMP_Text _attackGuidesMobile;
+    [SerializeField] private TMP_Text _attackGuidesCommon;
+    [Space]
     [SerializeField] private Button _okButton;
+    [SerializeField] private TMP_Text _start;
+    [SerializeField] private TMP_Text _next;
 
     private float _hintDuration = 0.5f;
-    private int _hintLoops = 8;
+    private int _hintLoops = -1;
+    private int _clickCounts;
+
+    private float _textFadeDuration = 1;
+    private float _textPause = 1.5f;
 
     private void Start()
     {
-        Invoke (nameof(ShowSideHints), 0.5f);
+        DOVirtual.DelayedCall(0.5f, () =>
+        {
+            Time.timeScale = 0;
+
+            _playerGuides.SetActive(true);
+
+            Fade(_targetGuides, 1, 1);
+
+            FadeOut(_missionPad);            
+
+            DOVirtual.DelayedCall(_textPause, () => Fade(_bigboxGuides, 1, _textFadeDuration));
+        });
 
         //AnimateFade(_coinPad, 0, _hintDuration, _hintLoops);
     }
@@ -46,82 +62,77 @@ public class TutorialPanel : MonoBehaviour
     }
 
     private void OnClickButton()
-    {
-        Time.timeScale = 1;
-
-        _playerGuides.gameObject.SetActive(false);
-    }
-
-    private void ShowSideHints()
     {        
-        float interval = _hintDuration * _hintLoops + 1;
-
-        SetText();
-
-        Time.timeScale = 0;
-
-        Sequence hintSequence = DOTween.Sequence();
-
-        hintSequence.SetUpdate(true);
-
-        hintSequence.AppendCallback(() => 
-        { 
-            AnimateMissionHint(_coinArrow, _coinText);
-            FadeOut(_coinPad, 0);
-        });
-        hintSequence.AppendInterval(interval);
-        hintSequence.AppendCallback(() =>
+        if (_clickCounts == 0)
         {
-            AnimateMissionHint(_killArrow, _killText);
-            FadeOut(_killPad, 0);
-        });
-        hintSequence.AppendInterval(interval);
-        hintSequence.AppendCallback(() =>
-        {
-            AnimateMissionHint(_bigboxArrow, _bigboxText);
-            FadeOut(_bigboxPad, 0);
-            FadeOut(_bigboxMark, 0);
-        });
-        hintSequence.AppendInterval(interval);
-        hintSequence.AppendCallback(() => _playerGuides.gameObject.SetActive(true));
+            _next.gameObject.SetActive(false);
 
-        //hintSequence.AppendCallback(() => Time.timeScale = 1);
+            _start.gameObject.SetActive(true);
+
+            _targetGuides.gameObject.SetActive(false);            
+
+            _bigboxGuides.gameObject.SetActive(false);
+
+            _missionPad.gameObject.SetActive(false);            
+
+            if (DataHandler.Instance.IsMobile())            {
+
+                /*_attackGuidesMobile.gameObject.SetActive(true);
+
+                _walkGuidesMobile.gameObject.SetActive(true);*/
+
+                DOVirtual.DelayedCall(_textPause, () => Fade(_walkGuidesMobile, 1, _textFadeDuration));                
+
+                DOVirtual.DelayedCall(_textPause * 2, () => Fade(_attackGuidesMobile, 1, _textFadeDuration));
+
+            }
+            else
+            {
+                /*_attackGuides.gameObject.SetActive(true);
+
+                _walkGuides.gameObject.SetActive(true);*/
+
+                DOVirtual.DelayedCall(_textPause, () => Fade(_walkGuides, 1, _textFadeDuration));
+
+                DOVirtual.DelayedCall(_textPause * 2, () => 
+                {
+                    Fade(_attackGuides, 1, _textFadeDuration);
+                    FadeOut(_attackPad);
+                });
+
+            }
+
+            //_attackGuidesCommon.gameObject.SetActive(true);
+
+            DOVirtual.DelayedCall(_textPause * 3, () => Fade(_attackGuidesCommon, 1, _textFadeDuration));
+
+            _clickCounts++;
+        }
+
+        else if (_clickCounts >= 1)
+        {
+            _playerGuides.SetActive(false);
+
+            _infoPanel.gameObject.SetActive(true);
+
+            Time.timeScale = 1;
+        }        
     }
 
-    private void AnimateMissionHint(Image arrow, TMP_Text text)
-    {
-        Vector3 startPosition = arrow.transform.position;
-        float offsetX = 150;
-        float offsetY = 120;
-
-        arrow.gameObject.SetActive(true);
-
-        text.gameObject.SetActive(true);
-
-        Vector3 targetPosition = new Vector3(startPosition.x + offsetX, startPosition.y + offsetY, 0);
-
-        arrow.transform.DOMove(targetPosition, _hintDuration).SetLoops(_hintLoops, LoopType.Yoyo).OnComplete(() =>
-        {
-            arrow.gameObject.SetActive(false);
-            text.gameObject.SetActive(false);
-            arrow.transform.position = startPosition;
-        }).SetUpdate(true);
-    }
-
-    private void FadeOut(Image image, float endAlpha)
+    private void FadeOut(Image image)
     {
         image.gameObject.SetActive(true);
 
-        image.DOFade(endAlpha, _hintDuration).SetLoops(_hintLoops, LoopType.Yoyo).OnComplete(() =>
+        image.DOFade(0, _hintDuration).SetLoops(_hintLoops, LoopType.Yoyo).OnComplete(() =>
         {
             image.gameObject.SetActive(false);
         }).SetUpdate(true);
     }
 
-    private void SetText()
+    private void Fade(TMP_Text text, float alpha, float duration)
     {
-        _coinText.text = $"Crush small boxes and collect {_questPanel.NeedCoinCollected.ToString()} coins";
+        text.gameObject.SetActive(true);
 
-        _killText.text = $"Kill {_questPanel.NeedEnemyKilled.ToString()} enemies";
+        text.DOFade(alpha, _hintDuration).SetUpdate(true).SetEase(Ease.InSine).SetUpdate(true);
     }
 }
