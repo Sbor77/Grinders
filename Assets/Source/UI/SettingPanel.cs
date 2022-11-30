7ce661993b388c50263f411738ed685a8f91ccdc
Sergey Borovkov <sborovkov82@mail.ru>
2022-11-30 22:19:57 +0300

    Debug fixed

--- Assets/Source/UI/SettingPanel.cs
+++ Assets/Source/UI/SettingPanel.cs
@@ -18,26 +18,27 @@ public class SettingPanel : MonoBehaviour
 
     private void OnEnable()
     {
-        //_masterVolumeSlider.onValueChanged.AddListener(OnMasterVolumeChanged);
-        //_musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
-        //_effectsVolumeSlider.onValueChanged.AddListener(OnEffectsVolumeChanged);
+        _masterVolumeSlider.onValueChanged.AddListener(OnMasterVolumeChanged);
+        _musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+        _effectsVolumeSlider.onValueChanged.AddListener(OnEffectsVolumeChanged);
     }
 
     private void OnDisable()
     {
-        //_masterVolumeSlider.onValueChanged.RemoveListener(OnMasterVolumeChanged);
-        //_musicVolumeSlider.onValueChanged.RemoveListener(OnMusicVolumeChanged);
-        //_effectsVolumeSlider.onValueChanged.RemoveListener(OnEffectsVolumeChanged);
+        _masterVolumeSlider.onValueChanged.RemoveListener(OnMasterVolumeChanged);
+        _musicVolumeSlider.onValueChanged.RemoveListener(OnMusicVolumeChanged);
+        _effectsVolumeSlider.onValueChanged.RemoveListener(OnEffectsVolumeChanged);
     }
 
     public void Start()
     {
-        //_masterVolumeSlider.value = DataHandler.Instance.GetSavedMasterVolume();
-        //_musicVolumeSlider.value = DataHandler.Instance.GetSavedMusicVolume();
-        //_effectsVolumeSlider.value = DataHandler.Instance.GetSavedEffectsVolume();
-        //OnMasterVolumeChanged(_masterVolumeSlider.value);
-        //OnMusicVolumeChanged(_musicVolumeSlider.value);
-        //OnEffectsVolumeChanged(_effectsVolumeSlider.value);
+        _masterVolumeSlider.value = DataHandler.Instance.GetSavedMasterVolume();
+        _musicVolumeSlider.value = DataHandler.Instance.GetSavedMusicVolume();
+        _effectsVolumeSlider.value = DataHandler.Instance.GetSavedEffectsVolume();
+        
+        OnMasterVolumeChanged(_masterVolumeSlider.value);
+        OnMusicVolumeChanged(_musicVolumeSlider.value);
+        OnEffectsVolumeChanged(_effectsVolumeSlider.value);
     }
 
     private void OnMasterVolumeChanged(float value)
