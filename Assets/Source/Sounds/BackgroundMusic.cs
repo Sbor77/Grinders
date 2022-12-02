7e7b9912db4f9a244d9ca37e76bf09fb11da5ca7
Sergey Borovkov <sborovkov82@mail.ru>
2022-12-02 14:36:43 +0300

    Improve background music logic

--- Assets/Source/Sounds/BackgroundMusic.cs
+++ Assets/Source/Sounds/BackgroundMusic.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackgroundMusic : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private List<AudioClip> _playList;    
+    [SerializeField] private List<AudioClip> _playList;
     
+    private int _shopSceneIndex = 5;    
+    private int _currentSceneIndex;
+    private Coroutine _playJob;
+    public bool _isPlaying;
+
     public static BackgroundMusic Instance { get; private set; }
-
-    public bool IsPlaying { get; private set; }
-
-    public int CurrentTrackIndex { get; private set; }
-
+    
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -27,20 +29,51 @@ public class BackgroundMusic : MonoBehaviour
         }
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        IsPlaying = true;
-
-        int clipIndex = DataHandler.Instance.GetSavedLevel();        
-
-        StartCoroutine(PlayMusicFrom(clipIndex));
+        SceneManager.activeSceneChanged += OnActiveSceneChanged;
     }
 
-    private IEnumerator PlayMusicFrom (int clipIndex)
+    private void OnDisable()
+    {
+        SceneManager.activeSceneChanged -= OnActiveSceneChanged;
+    }
+
+    private void Start()
+    {
+        ActivatePlayCoroutine();
+    }
+
+    private void OnActiveSceneChanged(Scene replacedScene, Scene newScene)
+    {
+        if (_playJob != null && newScene.buildIndex != _shopSceneIndex)
+        {
+            StopCoroutine(_playJob);
+
+            _playJob = null;
+
+            ActivatePlayCoroutine();
+        }
+    }
+
+    private void ActivatePlayCoroutine()
+    {
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        _isPlaying = true;
+
+        //int startClipIndex = _currentSceneIndex;
+
+        //_playJob = StartCoroutine(PlayMusicFrom(startClipIndex));
+
+        _playJob = StartCoroutine(PlayMusicFromCurrentSceneIndex());
+    }
+
+    private IEnumerator PlayMusicFromCurrentSceneIndex()
     {
         yield return null;
 
-        while (IsPlaying)
+        while (_isPlaying)
         {
             bool isFirstCycle = true;
 
@@ -48,11 +81,57 @@ public class BackgroundMusic : MonoBehaviour
             {
                 if (isFirstCycle)
                 {
+                    _audioSource.clip = _playList[_currentSceneIndex];
+
+                    i = _currentSceneIndex;
+
+                    isFirstCycle = false;
+                }
+                else
+                {
+                    _audioSource.clip = _playList[i];
+                }
+
+                _audioSource.Play();
+
+                while (_audioSource.isPlaying)
+                {
+                    yield return null;
+                }
+
+                if (_currentSceneIndex != SceneManager.GetActiveScene().buildIndex || _currentSceneIndex != _shopSceneIndex)
+                {
+                    _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+                    isFirstCycle = true;
+                }
+
+                if (i == _playList.Count - 1)
+                    i = -1;
+            }
+        }
+    }
+
+
+    /*private IEnumerator PlayMusicFrom (int clipIndex)
+    {
+        yield return null;
+
+        while (_isPlaying)
+        {
+            bool isFirstCycle = true;
+
+            for (int i = 0; i < _playList.Count; i++)
+            {
+                if (isFirstCycle)
+                {
+                    clipIndex = _currentSceneIndex;
+
                     _audioSource.clip = _playList[clipIndex];                    
 
                     i = clipIndex;
 
-                    isFirstCycle = false;
+                    isFirstCycle = false;                    
                 }
                 else
                 {
@@ -64,11 +143,18 @@ public class BackgroundMusic : MonoBehaviour
                 while(_audioSource.isPlaying)
                 {
                     yield return null;
-                }
+                }                
+
+                if (_currentSceneIndex != SceneManager.GetActiveScene().buildIndex || _currentSceneIndex != _shopSceneIndex)
+                {
+                    _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+                    isFirstCycle = true;
+                }                
 
                 if (i == _playList.Count - 1)
                     i = -1;
             }
         }
-    }
+    }*/
 }
