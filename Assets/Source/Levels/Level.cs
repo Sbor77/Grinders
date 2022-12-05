1001dc7978b98aafd6d38e754245c39079b271ce
Sergey Borovkov <sborovkov82@mail.ru>
2022-12-05 17:56:29 +0300

    fix localization 2.1

--- Assets/Source/Levels/Level.cs
+++ Assets/Source/Levels/Level.cs
@@ -7,7 +7,7 @@ using Lean.Localization;
 
 public class Level : MonoBehaviour
 {
-    [SerializeField] private LeanLocalization _leanLocalization;
+    //[SerializeField] private LeanLocalization _leanLocalization;
     [SerializeField] private Player _player;
     [SerializeField] private InfoViewer _infoViewer;
     [SerializeField] private EffectHandler _finalEffects;
--- Assets/Source/UI/Shop/Shop.cs
+++ Assets/Source/UI/Shop/Shop.cs
@@ -15,7 +15,7 @@ public class Shop : MonoBehaviour
 
     private void Start()
     {
-        _leanLocalization.SetCurrentLanguage(DataHandler.Instance.GetSavedLanguage());
+        //_leanLocalization.SetCurrentLanguage(DataHandler.Instance.GetSavedLanguage());
         GamesSdk.Instance.SetLeaderboardScore(DataHandler.Instance.GetSavedTotalScore());
         OnStatBought();
         GamesSdk.Instance.InterstitialAdShow();
