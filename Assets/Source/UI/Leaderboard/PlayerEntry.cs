729e235d238b17c49a262bfd7f0e0c59740618c2
Aleksandr Bashkirov <alxx85@mail.ru>
2022-12-15 06:08:16 +0700

    Fix leader view

--- Assets/Source/UI/Leaderboard/PlayerEntry.cs
+++ Assets/Source/UI/Leaderboard/PlayerEntry.cs
@@ -7,12 +7,12 @@ public class PlayerEntry : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _name;
     [SerializeField] private TextMeshProUGUI _score;
-    [SerializeField] private TextMeshProUGUI _place;
+    //[SerializeField] private TextMeshProUGUI _place;
 
     public void Render(string name, string score, string place)
     {
-        _name.text = name;
+        _name.text = $"{place} {name}";
         _score.text = score;
-        _place.text = place;
+        //_place.text = place;
     }
 }
